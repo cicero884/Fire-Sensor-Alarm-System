30b2a9a837b270a63826282b5031d6fe8ad14ce2
NightKirie <qwer55113322@gmail.com>
2019-03-03 02:37:02 +0800

    Add APP folder, to store the unity app
    Now can use camera, but it seems to have some bug, the camera won't open at the first time

---
+++ App/New Unity Project (2)/Assets/Scripts/Menu/Btn_Img_Gif.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Btn_Img_Gif : MonoBehaviour
+{
+    Image img;
+    
+    int frameCount = 0;
+    Sprite img1;
+
+    [SerializeField]int frameRate;
+    [SerializeField]Sprite img2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+        img1 = img.sprite; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ++frameCount;
+        if (frameCount == frameRate)
+            img.sprite = img2;
+        else if(frameCount == frameRate * 2)
+        {
+            img.sprite = img1;
+            frameCount = 0;
+        }
+    }
+}
