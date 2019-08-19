08f95f7158ac7e9fe54f8fa0c162e6a4f1a613cc
NightKirie <qwer55113322@gmail.com>
2019-08-19 22:04:50 +0800

    Simplify LinkToOtherLogin

--- Client/src/components/Login/LinkToOtherLogin.tsx
+++ Client/src/components/Login/LinkToOtherLogin.tsx
@@ -1,33 +1,28 @@
 import * as React from 'react'
 import { View, Text } from 'react-native'
 import styled from 'styled-components'
-import { Link } from '../../../components/Router'
+import { Link } from '../Router'
 
 const Label = styled(Text)`
-  font-family: AvantGardePro;
-  color: var(--light-grayish-blue);
+  	font-family: AvantGardePro;
+  	color: var(--light-grayish-blue);
 `
 
 const LinkToOtherLogin = (props: { isFireFighter: boolean }) => (
-  <View style={{ marginTop: 50 }}>
-    {props.isFireFighter ? (
-      <Label>
-        Looking for{' '}
-        <Link to="/citizen/login">
-          <Label>tenant login</Label>
-        </Link>
-        ?
-      </Label>
-    ) : (
-      <Label>
-        Looking for{' '}
-        <Link to="/firefighter/citizen/login">
-          <Label>landlord login</Label>
-        </Link>
-        ?
-      </Label>
-    )}
-  </View>
+	<View style={{ marginTop: 50 }}>
+		<Label>
+			Looking for{' '}
+			{props.isFireFighter ? (
+				<Link to="/citizen/login">
+					<Label>citizen login</Label>
+				</Link>
+			) : (
+					<Link to="/firefighter/citizen/login">
+						<Label>firefighter login</Label>
+					</Link>
+				)}
+			?</Label>
+	</View>
 )
 
 export default LinkToOtherLogin
