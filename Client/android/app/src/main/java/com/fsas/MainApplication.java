ecd8d7cc34b68927e5b34fb35c292b50cacbbc27
NightKirie <qwer55113322@gmail.com>
2019-08-30 01:24:05 +0800

    Adjust icons and layout

--- Client/android/app/src/main/java/com/fsas/MainApplication.java
+++ Client/android/app/src/main/java/com/fsas/MainApplication.java
@@ -3,6 +3,9 @@ package com.fsas;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;                       
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -28,6 +31,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseMessagingPackage(),
+            new RNFirebaseNotificationsPackage(),
             new VectorIconsPackage(),
             new ReanimatedPackage(),
             new MapsPackage(),
--- Client/src/components/Navigator.js
+++ Client/src/components/Navigator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text, Image } from 'react-native';
 import {  
     createSwitchNavigator,  
     createStackNavigator,
@@ -24,78 +25,213 @@ const LoginPageStackNavigator = createStackNavigator({
     transitionConfig: () => fromRight()
 });
 
-/* Conbine CitizenScan, CitizenBuilding, CitizenData in Citizen */
-const CitizenBottomTabNavigator = createBottomTabNavigator({
+
+/* Put CitizenScan into StackNavigator to get header */
+const CitizenScanPageStackNavigator = createStackNavigator({
     CitizenScanPage: {
         screen: CitizenScanPage,
         navigationOptions: {
-            tabBarIcon: (
-                <Icon 
-                    name='view-grid'
-                    type="material-community"
-                    color='#5BC100'/>
+            headerLeft: null,
+            headerTitle: ( 
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ justifyContent: 'center'}}>
+                        <Text style={{ fontSize: 40, fontWeight: 'bold', color: '#5BC100', fontFamily: 'sans-serif-condensed'}}>FSAS</Text>
+                    </View>
+                        <Image
+                            resizeMode="contain"
+                            source={require('../assets/icons/FSAS.png')}
+                            style={{ width: 40, height: 40, tintColor: "#5BC100" }} />
+                </View>
             ),
-        },  
+        },
     },
+},{
+    headerLayoutPreset: 'center'
+})
+
+/* Put CitizenBuilding into StackNavigator to get header */
+const CitizenBuildingPageStackNavigator = createStackNavigator({
     CitizenBuildingPage: {
         screen: CitizenBuildingPage,
         navigationOptions: {
-            tabBarIcon: (
-                <Icon 
-                    name='bell'
-                    type="material-community"
-                    color='#5BC100'/>
+            headerLeft: null,
+            headerTitle: ( 
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ justifyContent: 'center'}}>
+                        <Text style={{ fontSize: 40, fontWeight: 'bold', color: '#5BC100', fontFamily: 'sans-serif-condensed'}}>FSAS</Text>
+                    </View>
+                        <Image
+                            resizeMode="contain"
+                            source={require('../assets/icons/FSAS.png')}
+                            style={{ width: 40, height: 40, tintColor: "#5BC100" }} />
+                </View>
             ),
-        }, 
+        },
     },
+},{
+    headerLayoutPreset: 'center'
+})
+
+/* Put CitizenDataPage into StackNavigator to get header */
+const CitizenDataPageStackNavigator = createStackNavigator({
     CitizenDataPage: {
         screen: CitizenDataPage,
         navigationOptions: {
+            headerLeft: null,
+            headerTitle: ( 
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ justifyContent: 'center'}}>
+                        <Text style={{ fontSize: 40, fontWeight: 'bold', color: '#5BC100', fontFamily: 'sans-serif-condensed'}}>FSAS</Text>
+                    </View>
+                        <Image
+                            resizeMode="contain"
+                            source={require('../assets/icons/FSAS.png')}
+                            style={{ width: 40, height: 40, tintColor: "#5BC100" }} />
+                </View>
+            ),
+        },
+    },
+},{
+    headerLayoutPreset: 'center'
+})
+
+/* Conbine CitizenScan, CitizenBuilding, CitizenData in Citizen */
+const CitizenBottomTabNavigator = createBottomTabNavigator({
+    CitizenScanPageStackNavigator: {
+        screen: CitizenScanPageStackNavigator,
+        navigationOptions: {
+            tabBarLabel: (
+                <Text style={{ fontSize: 16, color: "#5BC100", textAlign: 'center'}}>掃描大樓</Text>
+            ),
+            tabBarIcon: (
+                <Icon 
+                    name='qrcode'
+                    type="material-community"
+                    size={30}
+                    color='#5BC100'/>
+            ),
+        }, 
+    },
+    CitizenBuildingPageStackNavigator: {
+        screen: CitizenBuildingPageStackNavigator,
+        navigationOptions: {
+            tabBarLabel: (
+                <Text style={{ fontSize: 16, color: "#5BC100", textAlign: 'center'}}>我的大樓</Text>
+            ),
+            tabBarIcon: (
+                <Icon 
+                    name='home-city'
+                    type="material-community"
+                    size={30}
+                    color='#5BC100'/>
+            ),
+        }, 
+    },
+    CitizenDataPageStackNavigator: {
+        screen: CitizenDataPageStackNavigator,
+        navigationOptions: {
+            tabBarLabel: (
+                <Text style={{ fontSize: 16, color: "#5BC100", textAlign: 'center'}}>我的資料</Text>
+            ),
             tabBarIcon: (
                 <Icon 
                     name='account'
                     type="material-community"
+                    size={30}
                     color='#5BC100'/>
             ),
         }, 
     },
 },{
-    initialRouteName: 'CitizenBuildingPage',
+    initialRouteName: 'CitizenBuildingPageStackNavigator',
     tabBarOptions: { 
-        showLabel: false,
         activeBackgroundColor: '#F2F1EF',
         inactiveBackgroundColor: '#FFFFFF',
     },
+
 });
 
-/* Conbine FirefighterBuilding, FirefighterData in Firefighter */
-const FirefighterBottomTabNavigator = createBottomTabNavigator({
+/* Put FirefighterBuildingPage into StackNavigator to get header */
+const FirefighterBuildingPageStackNavigator = createStackNavigator({
     FirefighterBuildingPage: {
         screen: FirefighterBuildingPage,
         navigationOptions: {
+            headerLeft: null,
+            headerTitle: ( 
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ justifyContent: 'center'}}>
+                        <Text style={{ fontSize: 40, fontWeight: 'bold', color: '#5BC100', fontFamily: 'sans-serif-condensed'}}>FSAS</Text>
+                    </View>
+                        <Image
+                            resizeMode="contain"
+                            source={require('../assets/icons/FSAS.png')}
+                            style={{ width: 40, height: 40, tintColor: "#5BC100" }} />
+                </View>
+            ),
+        },
+    },
+},{
+    headerLayoutPreset: 'center'
+})
+
+/* Put CitizenDataPage into StackNavigator to get header */
+const FirefighterDataPageStackNavigator = createStackNavigator({
+    FirefighterDataPage: {
+        screen: FirefighterDataPage,
+        navigationOptions: {
+            headerLeft: null,
+            headerTitle: ( 
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ justifyContent: 'center'}}>
+                        <Text style={{ fontSize: 40, fontWeight: 'bold', color: '#5BC100', fontFamily: 'sans-serif-condensed'}}>FSAS</Text>
+                    </View>
+                        <Image
+                            resizeMode="contain"
+                            source={require('../assets/icons/FSAS.png')}
+                            style={{ width: 40, height: 40, tintColor: "#5BC100" }} />
+                </View>
+            ),
+        },
+    },
+},{
+    headerLayoutPreset: 'center'
+})
+
+/* Conbine FirefighterBuilding, FirefighterData in Firefighter */
+const FirefighterBottomTabNavigator = createBottomTabNavigator({
+    FirefighterBuildingPageStackNavigator: {
+        screen: FirefighterBuildingPageStackNavigator,
+        navigationOptions: {
+            tabBarLabel: (
+                <Text style={{ fontSize: 16, color: "#5BC100", textAlign: 'center'}}>大樓清單</Text>
+            ),
             tabBarIcon: (
                 <Icon 
-                    name='bell'
+                    name='home-city'
                     type="material-community"
+                    size={30}
                     color='#5BC100'/>
             ),
         }, 
     },
-    FirefighterDataPage: {
-        screen: FirefighterDataPage,
+    FirefighterDataPageStackNavigator: {
+        screen: FirefighterDataPageStackNavigator,
         navigationOptions: {
+            tabBarLabel: (
+                <Text style={{ fontSize: 16, color: "#5BC100", textAlign: 'center'}}>我的資料</Text>
+            ),
             tabBarIcon: (
                 <Icon 
                     name='account'
                     type="material-community"
+                    size={30}
                     color='#5BC100'/>
             ),
         }, 
     },
 },{
-    initialRouteName: 'FirefighterBuildingPage',
+    initialRouteName: 'FirefighterBuildingPageStackNavigator',
     tabBarOptions: { 
-        showLabel: false,
         activeBackgroundColor: '#F2F1EF',
         inactiveBackgroundColor: '#FFFFFF',
     },
